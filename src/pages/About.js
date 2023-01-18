5e8e8779aa40a5835d255d0ac25ad66292738dd6
Michelle Chen <chensiqi96@gmail.com>
2023-01-17 20:58:36 -0800

    frontend complete

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import { useLoaderData } from "react-router-dom";
+
+
+function About(props){
+    const about = useLoaderData()
+
+    return <div>
+        <h2>{about.name}</h2>
+        <img src={about.headshot}/>
+        <h2>{about.email}</h2>
+        <p>{about.bio}</p>
+    </div>
+}
+
+export default About;
